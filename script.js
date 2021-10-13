bcaa7dc1575f1578c810e649f245666826ac91e2
panupongkeawkam <wpeoinrdb2544@gmail.com>
2021-10-13 18:20:52 +0700

    Change category elements

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $('.dropdown-toggle').dropdown()
 
+alert("yes")
+
 function setSearchBGAndBorder(BGColour, borerColour, position) {
     document.getElementsByClassName("search-box-container")[position].style.background = BGColour;
     document.getElementsByClassName("search-box-container")[position].style.border = `1px solid ${borerColour}`;
